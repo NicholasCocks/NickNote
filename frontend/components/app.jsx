97ee1fce992c4045c7c40cab976fd3461a475a78
nickc <nick@caveat.nyc>
2020-11-18 10:58:17 -0500

    user model name fixed

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -4,8 +4,10 @@ import { ProtectedRoute, AuthRoute } from '../util/route_util'
 import NotesAppContainer from './notes/notes_app_container';
 
 const App = () => {
+    //put switch statement so that they are redirected splash page
     return (
         <div>
+            
             <AuthRoute path="/session" component={SessionPage}/>
             <ProtectedRoute path="/notes" component={NotesAppContainer} />
         </div>
