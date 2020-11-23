f8b074abc59a47595d81a7f9fbf09c4dc3974206
nickc <nick@caveat.nyc>
2020-11-23 14:00:21 -0500

    renamed user model file

--- app/models/User.rb
+++ app/models/User.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
 
     attr_reader :password
 
-    after_initialize :ensure_session_token #after_create ensure_first_notebook
+    after_initialize :ensure_session_token 
 
     has_many :notes,
         foreign_key: :author_id,
